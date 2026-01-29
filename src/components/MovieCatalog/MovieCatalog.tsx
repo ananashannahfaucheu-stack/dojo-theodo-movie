9d72d16dac5572bca71287ce3470d9e953e5f664
ananashannahfaucheu-stack <ananas.hannahfaucheu@gmail.com>
2026-01-29 15:49:36 +0100

    version 1

--- src/components/MovieCatalog/MovieCatalog.tsx
+++ src/components/MovieCatalog/MovieCatalog.tsx
@@ -1,12 +1,15 @@
 import styles from "./MovieCatalog.module.css";
 
 import GenreList from "../GenreList/GenreList";
+import MoviePreview from "../MoviePreview/MoviePreview";
+import { mockMovie } from "../../mock/mockMovie";
 
-export const MovieCatalog = () => {
+export const MovieCatalog = ({ text }: { text: string }) => {
   return (
     <>
       <GenreList />
       <div className={styles.layout}></div>
+      <MoviePreview movie={mockMovie}/>
     </>
   );
 };
--- src/components/MoviePreview/MoviePreview.tsx
+++ src/components/MoviePreview/MoviePreview.tsx
@@ -1,11 +1,28 @@
+import { POSTER_URL_PREFIX } from "../../App";
+import { Movie } from "../../models";
 import styles from "./MoviePreview.module.css";
 
-const MoviePreview = () => {
+const MoviePreview = ({ movie }: { movie: Movie }) => {
   const goToMovieDetails = () => {
     // A DEFINIR EXPERT
   };
+  const imageUrl = `${POSTER_URL_PREFIX}${movie.poster_path}`;
 
-  return <div className={styles.movieCard} onClick={goToMovieDetails}></div>;
+  return (
+    <div className={styles.movieCard} onClick={goToMovieDetails}>
+      {/* 1. L'image du film */}
+      <img 
+        src={imageUrl} 
+        alt={`Affiche du film ${movie.title}`} 
+        className={styles.moviePoster} // Pensez à styliser cette classe en CSS
+      />
+
+      {/* 2. Le titre (optionnel mais recommandé pour savoir ce qu'on clique) */}
+      <div className={styles.movieInfo}>
+        <h3>{movie.title}</h3>
+      </div>
+    </div>
+  )
 };
 
 export default MoviePreview;
